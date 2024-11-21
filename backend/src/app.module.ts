bc73341f7199edc8e992021171403df71c4c3171
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-11-21 15:34:34 -0300

    fix(backend): import app controller and app service in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,18 @@ import { PrismaModule } from './prisma/prisma.module';
 import { PayableModule } from './modules/integrations/payable/payable.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [AssignorModule, PrismaModule, PayableModule, UserModule, AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    AssignorModule,
+    PrismaModule,
+    PayableModule,
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
