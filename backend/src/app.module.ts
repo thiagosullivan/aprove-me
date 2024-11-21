6e777afd45487ec7ea177161033ebc93d572aad4
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-11-21 15:49:03 -0300

    feat(backend): implement JWT validation on routes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,8 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [
@@ -16,6 +18,12 @@ import { AppService } from './app.service';
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
---
+++ backend/src/auth/models/UserFromJwt.ts
@@ -0,0 +1,5 @@
+export interface UserFromJwt {
+  id: string;
+  login: string;
+  name: string;
+}
