54bc25f99660711346c230ee3131994284eae24b
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-11-21 13:06:51 -0300

    feat(backend): implement login, guard and local strategy

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  validateUser(login: string, password: string) {
+    throw new Error('Method not implemented.');
+  }
+}
