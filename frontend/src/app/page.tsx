6101bc4d1de61a80e83f2a934e0adff25f0f5161
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-11-21 22:28:16 -0300

    feat(frontend): added new color

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p className="text-primary text-5xl font-bold">Bankme</p>
+      <p className="text-bankmeBlue text-5xl font-bold">Bankme</p>
     </div>
   );
 }
